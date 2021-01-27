edaee8bf76c8db261add82a6e6ee2bb289198dfc
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2021-01-27 15:52:52 +0100

    #1 znalezienie wartości minimalnej, refaktoring

--- src/MathExample.java
+++ src/MathExample.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 
 public class MathExample {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter a number: ");
         int size = scanner.nextInt();
 
-        double[] numbers = new double[size];
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
-        }
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = Math.random();  //metoda random zwraca zawsze doubla
-        }
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
+        NumberRepo numberRepo = new NumberRepo(size);
 
-        }
+        double[] numbers = new double[size];
+        numberRepo.generateValues();
+        numberRepo.displayAll();
+
+        double min = numberRepo.findMin();
+        System.out.println("Min value: "+min);
     }
 }
+
